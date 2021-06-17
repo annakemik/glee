e133036e6ddfe79adc17f1bd8672b283aec1135d
anutati <anutati@yahoo.com>
2021-06-16 22:30:56 -0400

    homework 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,25 @@
-$(function() {
-
+$(function () {
     $('.top-slider__inner').slick({
-dots: true,
-arrows: false,
+        dots: true,
+        arrows: false,
     });
+
+    $('.partners__slider').slick({
+        dots: true,
+        arrows: false,
+    });
+
+    var mixerProductWeek = mixitup('.product-week', {
+        controls: {
+            scope: 'local'
+        }
+    });
+
+    var mixerDesign = mixitup('.design', {
+        controls: {
+            scope: 'local'
+        }
+    });
+
 
 });
